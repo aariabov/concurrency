efa0a2c75c23f76e9ec04b81f19902db40d9387b
Riabov.AA <riabov.aa@parmalogica.ru>
2024-02-19 12:17:25 +0500

    ручная реализация асинхронных методов

---
+++ Tests/My/Manual/ManualImplTests.cs
@@ -0,0 +1,110 @@
+﻿using System.Net;
+
+namespace Tests.My.Manual;
+
+[TestClass]
+public class ManualImplTests
+{
+    [TestMethod]
+    public void имитация_нажатия_кнопки_и_обновления_интерфейса_через_callback()
+    {
+        var repoMock = new Repository();
+        var sut = new SutService(repoMock);
+        
+        sut.UpdateCount_BtnClick_Callback();
+        Thread.Sleep(1100);
+    }
+    
+    [TestMethod]
+    public void имитация_нажатия_кнопки_и_обновления_интерфейса_через_event()
+    {
+        var repoMock = new Repository();
+        var sut = new SutService(repoMock);
+        
+        sut.UpdateCount_BtnClick_Event();
+        Thread.Sleep(1100);
+    }
+    
+    [TestMethod]
+    public void имитация_нажатия_кнопки_и_обновления_интерфейса_через_async_result()
+    {
+        var repoMock = new Repository();
+        var sut = new SutService(repoMock);
+        
+        sut.UpdateCount_BtnClick_AsyncResult();
+        Thread.Sleep(1100);
+    }
+}
+
+public delegate void GetCountEventHandler(object sender, GetCountEventHandlerArgs args);
+
+public class GetCountEventHandlerArgs
+{
+    public int Count { get; set; }
+    public GetCountEventHandlerArgs(int count)
+    {
+        Count = count;
+    }
+}
+
+public class Repository : IRepository
+{
+    public event GetCountEventHandler? GetCountCompleted;
+    
+    public void GetCountOnEvent()
+    {
+        new Thread(() =>
+        {
+            Console.WriteLine($"Second start: {Thread.CurrentThread.ManagedThreadId}");
+            Thread.CurrentThread.IsBackground = true; 
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            Console.WriteLine($"Second end: {Thread.CurrentThread.ManagedThreadId}");
+            GetCountCompleted?.Invoke(this, new GetCountEventHandlerArgs(42));
+        }).Start();
+        Console.WriteLine($"Repo end: {Thread.CurrentThread.ManagedThreadId}");
+    }
+    
+    public void BeginGetCountAsyncCallback(AsyncCallback? callback, object unrelatedObject)
+    {
+        new Thread(() =>
+        {
+            Console.WriteLine($"Second start: {Thread.CurrentThread.ManagedThreadId}");
+            Thread.CurrentThread.IsBackground = true; 
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            Console.WriteLine($"Second end: {Thread.CurrentThread.ManagedThreadId}");
+            callback?.Invoke(new AsyncResult(unrelatedObject));
+        }).Start();
+        Console.WriteLine($"Repo end: {Thread.CurrentThread.ManagedThreadId}");
+    }
+
+    public int EndGetCountAsyncCallback(IAsyncResult result)
+    {
+        return 42;
+    }
+    
+    public void GetCount(Action<int> callback)
+    {
+        new Thread(() => 
+        {
+            Console.WriteLine($"Second start: {Thread.CurrentThread.ManagedThreadId}");
+            Thread.CurrentThread.IsBackground = true; 
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            Console.WriteLine($"Second end: {Thread.CurrentThread.ManagedThreadId}");
+            callback(42);
+        }).Start();
+        Console.WriteLine($"Repo end: {Thread.CurrentThread.ManagedThreadId}");
+    }
+}
+
+public class AsyncResult : IAsyncResult
+{
+    public AsyncResult(object? asyncState)
+    {
+        AsyncState = asyncState;
+    }
+
+    public object? AsyncState { get; }
+    public WaitHandle AsyncWaitHandle { get; }
+    public bool CompletedSynchronously { get; }
+    public bool IsCompleted { get; }
+}
---
+++ Tests/My/Manual/SutService.cs
@@ -0,0 +1,75 @@
+﻿using System.Net;
+
+namespace Tests.My.Manual;
+
+public interface IRepository
+{
+    void GetCount(Action<int> callback);
+    void BeginGetCountAsyncCallback(AsyncCallback? callback, object unrelatedObject);
+    int EndGetCountAsyncCallback(IAsyncResult result);
+    
+    void GetCountOnEvent();
+    public event GetCountEventHandler? GetCountCompleted;
+}
+
+public class SutService
+{
+    private readonly IRepository _repository;
+
+    public SutService(IRepository repository)
+    {
+        _repository = repository;
+    }
+
+    /// <summary>
+    /// Имитация обработчика кнопки: запускает длительную операцию и передает ей callback для обновления интерфейса
+    /// </summary>
+    public void UpdateCount_BtnClick_Callback()
+    {
+        Console.WriteLine($"Main start: {Thread.CurrentThread.ManagedThreadId}");
+        _repository.GetCount((count) =>
+        {
+            // обновляем интерфейс
+            Console.WriteLine($"Callback {count} шт.: {Thread.CurrentThread.ManagedThreadId}");
+        });
+        Console.WriteLine($"Main end: {Thread.CurrentThread.ManagedThreadId}");
+    }
+
+    /// <summary>
+    /// Имитация обработчика кнопки: запускает длительную операцию и подписывается на событие для обновления интерфейса
+    /// Недостатки: метод разбирается на 2, надо не забыть отписаться от события
+    /// </summary>
+    public void UpdateCount_BtnClick_Event()
+    {
+        Console.WriteLine($"Main start: {Thread.CurrentThread.ManagedThreadId}");
+        _repository.GetCountCompleted += (sender, args) =>
+        {
+            // обновляем интерфейс
+            Console.WriteLine($"Callback {args.Count} шт.: {Thread.CurrentThread.ManagedThreadId}");
+        };
+        _repository.GetCountOnEvent();
+        Console.WriteLine($"Main end: {Thread.CurrentThread.ManagedThreadId}");
+    }
+
+    /// <summary>
+    /// Имитация обработчика кнопки: запускает длительную операцию и использует AsyncResult
+    /// Недостатки: метод разбивается на 2, выглядит все не очень
+    /// </summary>
+    public void UpdateCount_BtnClick_AsyncResult()
+    {
+        Console.WriteLine($"Main start: {Thread.CurrentThread.ManagedThreadId}");
+        object unrelatedObject = "hello";
+        // запускает длительную операцию
+        _repository.BeginGetCountAsyncCallback(OnResolved, unrelatedObject);
+        Console.WriteLine($"Main end: {Thread.CurrentThread.ManagedThreadId}");
+    }
+    
+    private void OnResolved(IAsyncResult ar)
+    {
+        object unrelatedObject = ar.AsyncState;
+        // получает результат
+        var result = _repository.EndGetCountAsyncCallback(ar);
+        // Обработать адрес
+        Console.WriteLine($"Callback {result}шт. {unrelatedObject}: {Thread.CurrentThread.ManagedThreadId}");
+    }
+}
