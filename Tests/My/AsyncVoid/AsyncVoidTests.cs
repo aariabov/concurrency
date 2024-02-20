adb9cd52d1e6f0335e865711f9fa4ca12fbf47de
Riabov.AA <riabov.aa@parmalogica.ru>
2024-02-20 12:27:17 +0500

    async void tests

---
+++ Tests/My/AsyncVoid/AsyncVoidTests.cs
@@ -0,0 +1,65 @@
+﻿namespace Tests.My.AsyncVoid;
+
+[TestClass]
+public class AsyncVoidTests
+{
+    [TestMethod]
+    public void потеря_исключения_в_async_void()
+    {
+        var repoMock = new Repository();
+        var sut = new SutService(repoMock);
+        
+        sut.Delete();
+    }
+    
+    [TestMethod]
+    public void потеря_исключения_в_async_void_исключение_возникает_в_другом_потоке()
+    {
+        var repoMock = new Repository();
+        var sut = new SutService(repoMock);
+
+        sut.DeleteWithTryCatch();
+    }
+    
+    [TestMethod]
+    public async Task ловим_исключение_в_async_Task()
+    {
+        var repoMock = new Repository();
+        var sut = new SutService(repoMock);
+        
+        var func = () => sut.DeleteAsync();
+        await Assert.ThrowsExceptionAsync<NotImplementedException>(func);
+    }
+}
+
+public class Repository : IRepository
+{
+    public async void Delete()
+    {
+        Console.WriteLine($"Repo start: {Thread.CurrentThread.ManagedThreadId}");
+        await Task.Delay(TimeSpan.FromSeconds(1));
+        throw new NotImplementedException();
+    }
+    
+    public async void DeleteWithTryCatch()
+    {
+        try
+        {
+            Console.WriteLine($"Repo start: {Thread.CurrentThread.ManagedThreadId}");
+            await Task.Delay(TimeSpan.FromSeconds(1));
+            throw new NotImplementedException();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Repo exception: {Thread.CurrentThread.ManagedThreadId}");
+            Console.WriteLine(e);
+        }
+    }
+    
+    public async Task DeleteAsync()
+    {
+        Console.WriteLine($"Repo start: {Thread.CurrentThread.ManagedThreadId}");
+        await Task.Delay(TimeSpan.FromSeconds(1));
+        throw new NotImplementedException();
+    }
+}
---
+++ Tests/My/AsyncVoid/SutService.cs
@@ -0,0 +1,72 @@
+﻿namespace Tests.My.AsyncVoid;
+
+public interface IRepository
+{
+    void Delete();
+    void DeleteWithTryCatch();
+    Task DeleteAsync();
+}
+
+public class SutService
+{
+    private readonly IRepository _repository;
+
+    public SutService(IRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public void Delete()
+    {
+        try
+        {
+            Console.WriteLine($"Service start: {Thread.CurrentThread.ManagedThreadId}");
+            // не ждем завершения метода, поэтому не ловим исключение - оно возникает в другом потоке
+            _repository.Delete();
+            Console.WriteLine($"Service end: {Thread.CurrentThread.ManagedThreadId}");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Service exception: {Thread.CurrentThread.ManagedThreadId}");
+            Console.WriteLine(e);
+        }
+    }
+
+    public void DeleteWithTryCatch()
+    {
+        try
+        {
+            Console.WriteLine($"Service start: {Thread.CurrentThread.ManagedThreadId}");
+            // ждем пока сработает исключение и смотрим его поток
+            _repository.DeleteWithTryCatch();
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+            Console.WriteLine($"Service end: {Thread.CurrentThread.ManagedThreadId}");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Service exception: {Thread.CurrentThread.ManagedThreadId}");
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
+    public async Task DeleteAsync()
+    {
+        var task = _repository.DeleteAsync();
+        Console.WriteLine($"Task info: Status - {task.Status}, IsCompleted - {task.IsCompleted}");
+        try
+        {
+            Console.WriteLine($"Service start: {Thread.CurrentThread.ManagedThreadId}");
+            await task;
+            Console.WriteLine($"Service end: {Thread.CurrentThread.ManagedThreadId}");
+        }
+        catch (Exception e)
+        {
+            // исключение возникает в другом потоке
+            Console.WriteLine($"Service exception: {Thread.CurrentThread.ManagedThreadId}");
+            Console.WriteLine($"Task info: Status - {task.Status}, IsCompleted - {task.IsCompleted}");
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}
