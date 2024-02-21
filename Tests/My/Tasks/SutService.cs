00c6d156169a72460f38f650a9cd64cae09614ac
Riabov.AA <riabov.aa@parmalogica.ru>
2024-02-21 13:15:19 +0500

    TaskCompletionSource and Delay

--- Tests/My/Tasks/SutService.cs
+++ Tests/My/Tasks/SutService.cs
@@ -1,4 +1,7 @@
-﻿namespace Tests.My.Tasks;
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Tests.My.Tasks;
 
 public class SutService
 {
@@ -12,5 +15,55 @@ public class SutService
     {
         Console.WriteLine($"{nameof(Method2)}: {Thread.CurrentThread.ManagedThreadId}");
         return Thread.CurrentThread.ManagedThreadId;
+    }
+
+    public Task MyDelay(int milliseconds)
+    {
+        TaskCompletionSource taskCompletionSource = new TaskCompletionSource();
+        Thread.Sleep(milliseconds);
+        // говорим, что асинхронный метод завершился,
+        // если его не вызвать, то метод будет работать бесконечно
+        taskCompletionSource.SetResult();
+        return taskCompletionSource.Task;
+    }
+
+    public async Task TaskRunDelay(int milliseconds)
+    {
+        await Task.Run(() =>
+        {
+            Console.WriteLine($"TaskRunDelay: {Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep(milliseconds);
+        });
+    }
+
+    public Task TimerDelay(int milliseconds)
+    {
+        var tcs = new TaskCompletionSource<object>();
+        var timer = new Timer(_ =>
+        {
+            Console.WriteLine($"MyDelay1 SetResult: {Thread.CurrentThread.ManagedThreadId}");
+            tcs.SetResult(null);
+        }, null, milliseconds, Timeout.Infinite);
+        tcs.Task.ContinueWith(_ =>
+        {
+            Console.WriteLine($"MyDelay1 ContinueWith: {Thread.CurrentThread.ManagedThreadId}");
+            timer.Dispose();
+        });
+        Console.WriteLine($"MyDelay1 return: {Thread.CurrentThread.ManagedThreadId}");
+        return tcs.Task;
+    }
+    
+    public Task<byte[]> DownloadDataAsync(WebClient webClient, string uri)
+    {
+        var taskCompletionSource = new TaskCompletionSource<byte[]>();
+        // используем устаревший способ
+        webClient.DownloadDataCompleted += (sender, args) =>
+        {
+            // возвращаем результат современным способом
+            taskCompletionSource.SetResult(args.Result);
+        };
+        webClient.DownloadDataAsync(new Uri(uri));
+        
+        return taskCompletionSource.Task;
     }
 }
--- Tests/My/Tasks/TaskTests.cs
+++ Tests/My/Tasks/TaskTests.cs
@@ -1,4 +1,7 @@
-﻿namespace Tests.My.Tasks;
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Tests.My.Tasks;
 
 [TestClass]
 public class TaskTests
@@ -27,5 +30,45 @@ public class TaskTests
         Assert.IsInstanceOfType(method1ThreadId, typeof(int));
         Assert.IsInstanceOfType(method2ThreadId, typeof(int));
         Assert.AreNotEqual(method1ThreadId, method2ThreadId);
+    }
+    
+    [TestMethod]
+    public async Task my_delay()
+    {
+        var sut = new SutService();
+        
+        await sut.MyDelay(100);
+    }
+    
+    [TestMethod]
+    public async Task my_timer_delay()
+    {
+        var sut = new SutService();
+        
+        Console.WriteLine($"Test: {Thread.CurrentThread.ManagedThreadId}");
+        var task = sut.TimerDelay(100);
+        await task;
+    }
+    
+    [TestMethod]
+    public async Task task_run_delay()
+    {
+        var sut = new SutService();
+        
+        Console.WriteLine($"Test: {Thread.CurrentThread.ManagedThreadId}");
+        var task = sut.TaskRunDelay(100);
+        await task;
+    }
+    
+    [TestMethod]
+    public async Task task_completion_source_example()
+    {
+        var sut = new SutService();
+        // допустим, у нас во всем приложении используется устаревший WebClient
+        // и мы хотим перейти на православный async/await
+        var webClient = new WebClient();
+
+        var bytes = await sut.DownloadDataAsync(webClient, "http://ya.ru/favicon.ico");
+        Assert.IsTrue(bytes.Length > 0);
     }
 }
