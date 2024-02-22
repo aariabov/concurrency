148e5c0ad3890a0b5ad5d0c4d93e14a577a990c4
Riabov.AA <riabov.aa@parmalogica.ru>
2024-02-22 09:23:49 +0500

    WhenAll tests

---
+++ Tests/My/Tasks/WhenAll/SutService.cs
@@ -0,0 +1,73 @@
+﻿namespace Tests.My.Tasks.WhenAll;
+
+public class SutService
+{
+    
+    
+    public async Task<int> WhenAllSum(int from, int to)
+    {
+        Console.WriteLine($"WhenAll: {Thread.CurrentThread.ManagedThreadId}");
+        var tasks = new List<Task<int>>();
+        foreach (var i in Enumerable.Range(from, to))
+        {
+            var task = Task.Run(() =>
+            {
+                Console.WriteLine($"Task{i}: {Thread.CurrentThread.ManagedThreadId}");
+                return i;
+            });
+            tasks.Add(task);
+        }
+
+        var values = await Task.WhenAll(tasks);
+        Console.WriteLine($"WhenAll: {Thread.CurrentThread.ManagedThreadId}");
+        return values.Sum();
+    }
+    
+    public async Task<int> WhenAllFirstException(int from, int to)
+    {
+        Console.WriteLine($"WhenAll: {Thread.CurrentThread.ManagedThreadId}");
+        var tasks = new List<Task<int>>();
+        foreach (var i in Enumerable.Range(from, to))
+        {
+            var task = Task.Run(() =>
+            {
+                Console.WriteLine($"Task{i}: {Thread.CurrentThread.ManagedThreadId}");
+                throw new TimeoutException();
+                return 1;
+            });
+            tasks.Add(task);
+        }
+
+        var values = await Task.WhenAll(tasks);
+        Console.WriteLine($"WhenAll: {Thread.CurrentThread.ManagedThreadId}");
+        return values.Sum();
+    }
+    
+    public async Task<int> WhenAllCatchAllExceptions(int from, int to)
+    {
+        Console.WriteLine($"WhenAll: {Thread.CurrentThread.ManagedThreadId}");
+        var tasks = new List<Task<int>>();
+        foreach (var i in Enumerable.Range(from, to))
+        {
+            var task = Task.Run(() =>
+            {
+                Console.WriteLine($"Task{i}: {Thread.CurrentThread.ManagedThreadId}");
+                throw new TimeoutException();
+                return 1;
+            });
+            tasks.Add(task);
+        }
+        var whenAllTask = Task.WhenAll(tasks);
+
+        try
+        {
+            var values = await whenAllTask;
+            Console.WriteLine($"WhenAll: {Thread.CurrentThread.ManagedThreadId}");
+            return values.Sum();
+        }
+        catch (Exception)
+        {
+            throw whenAllTask.Exception;
+        }
+    }
+}
---
+++ Tests/My/Tasks/WhenAll/WhenAllTests.cs
@@ -0,0 +1,32 @@
+﻿namespace Tests.My.Tasks.WhenAll;
+
+[TestClass]
+public class WhenAllTests
+{
+    [TestMethod]
+    public async Task when_all_sum()
+    {
+        var sut = new SutService();
+
+        var result = await sut.WhenAllSum(1, 3);
+        Assert.AreEqual(6, result);
+    }
+    
+    [TestMethod]
+    public async Task when_all_first_exception()
+    {
+        var sut = new SutService();
+
+        var func = () => sut.WhenAllFirstException(1, 3);
+        await Assert.ThrowsExceptionAsync<TimeoutException>(func);
+    }
+    
+    [TestMethod]
+    public async Task when_all_catch_all_exceptions()
+    {
+        var sut = new SutService();
+
+        var func = () => sut.WhenAllCatchAllExceptions(1, 3);
+        await Assert.ThrowsExceptionAsync<AggregateException>(func);
+    }
+}
