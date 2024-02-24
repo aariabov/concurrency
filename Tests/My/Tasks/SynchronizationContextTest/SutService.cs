077ad218c0d60a9415017d5249da5a5af7d5baaa
Riabov.AA <riabov.aa@parmalogica.ru>
2024-02-24 12:01:50 +0500

    SynchronizationContext

---
+++ Tests/My/Tasks/SynchronizationContextTest/SutService.cs
@@ -0,0 +1,22 @@
+﻿namespace Tests.My.Tasks.SynchronizationContextTest;
+
+public class SutService
+{
+    public async Task ContinueWithSameThread()
+    {
+        Console.WriteLine($"Init thread id: {Thread.CurrentThread.ManagedThreadId}");
+        await Task.Delay(100);
+        Console.WriteLine($"Continue thread id: {Thread.CurrentThread.ManagedThreadId}");
+    }
+    
+    public async Task TaskDelay()
+    {
+        Console.WriteLine($"Start: {Thread.CurrentThread.ManagedThreadId}");
+        // await Task.Delay(100).ContinueWith(task =>
+        // {
+        //     Console.WriteLine($"End: {Thread.CurrentThread.ManagedThreadId}");
+        // });
+        await Task.Delay(100);
+        Console.WriteLine($"End: {Thread.CurrentThread.ManagedThreadId}");
+    }
+}
---
+++ Tests/My/Tasks/SynchronizationContextTest/SynchronizationContextTests.cs
@@ -0,0 +1,60 @@
+﻿using Nito.AsyncEx;
+
+namespace Tests.My.Tasks.SynchronizationContextTest;
+
+[TestClass]
+public class SynchronizationContextTests
+{
+    [TestMethod]
+    public async Task продолжить_в_том_же_потоке()
+    {
+        // используется библиотека для задания контекста
+        AsyncContext.Run(async () =>
+        {
+            var sut = new SutService();
+        
+            await sut.ContinueWithSameThread();
+        });
+    }
+    
+    [TestMethod]
+    public async Task synchronization_context_is_null()
+    {
+        Assert.IsNull(SynchronizationContext.Current);
+    }
+    
+    [TestMethod]
+    public async Task вызов_post_после_завершения_асинхронного_метода()
+    {
+        var mockContext = new MockSynchronizationContext();
+        SynchronizationContext.SetSynchronizationContext(mockContext);
+        
+        await Task.Delay(100);
+        
+        Assert.AreEqual(1, mockContext.PostCallTimes);
+    }
+    
+    [TestMethod]
+    public async Task вызов_post_после_завершения_2х_асинхронных_методов()
+    {
+        var mockContext = new MockSynchronizationContext();
+        SynchronizationContext.SetSynchronizationContext(mockContext);
+
+        var sut = new SutService();
+        await sut.TaskDelay();
+        
+        // 2 раза, тк есть продолжение в асинхронном методе и в самом тесте
+        Assert.AreEqual(2, mockContext.PostCallTimes);
+    }
+}
+
+public class MockSynchronizationContext : SynchronizationContext
+{
+    public int PostCallTimes { get; set; }
+
+    public override void Post(SendOrPostCallback callback, object? state)
+    {
+        PostCallTimes++;
+        callback(state);
+    }
+}
