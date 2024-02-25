977086ec867a622848cef023e287bac60aa7a286
Riabov.AA <riabov.aa@parmalogica.ru>
2024-02-25 14:32:49 +0500

    сравнение продолжений в исходном потоке и в произвольном

--- Tests/My/Tasks/SynchronizationContextTest/SynchronizationContextTests.cs
+++ Tests/My/Tasks/SynchronizationContextTest/SynchronizationContextTests.cs
@@ -1,4 +1,5 @@
-﻿using Nito.AsyncEx;
+﻿using System.Diagnostics;
+using Nito.AsyncEx;
 
 namespace Tests.My.Tasks.SynchronizationContextTest;
 
@@ -45,6 +46,139 @@ public class SynchronizationContextTests
         
         // 2 раза, тк есть продолжение в асинхронном методе и в самом тесте
         Assert.AreEqual(2, mockContext.PostCallTimes);
+    }
+    
+    [TestMethod]
+    public async Task запуск_асинхронных_методов_без_родительского_контекста()
+    {
+        var mainThreadId = Thread.CurrentThread.ManagedThreadId;
+        Console.WriteLine("Main: " + Thread.CurrentThread.ManagedThreadId);
+        Assert.AreEqual(null, SynchronizationContext.Current);
+        await Task.Run(() =>
+        {
+            Console.WriteLine($"Task run 1: {Thread.CurrentThread.ManagedThreadId}");
+        });
+        // продолжение выполняется в потоке из ThreadPool
+        Assert.AreNotEqual(mainThreadId, Thread.CurrentThread.ManagedThreadId);
+        Console.WriteLine($"Продолжение1: {Thread.CurrentThread.ManagedThreadId}, {SynchronizationContext.Current}\n");
+        
+        await Task.Run(() =>
+        {
+            Console.WriteLine("Task run 2: " + Thread.CurrentThread.ManagedThreadId);
+        });
+        Assert.AreNotEqual(mainThreadId, Thread.CurrentThread.ManagedThreadId);
+        // продолжение выполняется в потоке из ThreadPool
+        Console.WriteLine($"Продолжение2: {Thread.CurrentThread.ManagedThreadId}, {SynchronizationContext.Current}\n");
+    }
+    
+    [TestMethod]
+    public async Task продолжение_выполняется_в_родительском_потоке()
+    {
+        AsyncContext.Run(async () =>
+        {
+            var mainThreadId = Thread.CurrentThread.ManagedThreadId;
+            Console.WriteLine("Main: " + Thread.CurrentThread.ManagedThreadId);
+            Assert.AreNotEqual(null, SynchronizationContext.Current);
+            await Task.Run(() =>
+            {
+                Console.WriteLine($"Task run 1: {Thread.CurrentThread.ManagedThreadId}");
+            });
+            // продолжение выполняется в главном потоке
+            Assert.AreEqual(mainThreadId, Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine($"Продолжение1: {Thread.CurrentThread.ManagedThreadId}, {SynchronizationContext.Current}\n");
+        
+            await Task.Run(() =>
+            {
+                Console.WriteLine("Task run 2: " + Thread.CurrentThread.ManagedThreadId);
+            });
+            // продолжение выполняется в главном потоке
+            Assert.AreEqual(mainThreadId, Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine($"Продолжение2: {Thread.CurrentThread.ManagedThreadId}, {SynchronizationContext.Current}\n");
+        });
+    }
+    
+    [TestMethod]
+    public async Task продолжение_выполняется_в_потоке_из_thread_pool()
+    {
+        AsyncContext.Run(async () =>
+        {
+            var mainThreadId = Thread.CurrentThread.ManagedThreadId;
+            Console.WriteLine("Main: " + Thread.CurrentThread.ManagedThreadId);
+            Assert.AreNotEqual(null, SynchronizationContext.Current);
+            await Task.Run(() =>
+            {
+                Console.WriteLine($"Task run 1: {Thread.CurrentThread.ManagedThreadId}");
+            }).ConfigureAwait(false);
+            // продолжение выполняется в потоке из ThreadPool
+            Assert.AreNotEqual(mainThreadId, Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine($"Продолжение1: {Thread.CurrentThread.ManagedThreadId}, {SynchronizationContext.Current}\n");
+        
+            await Task.Run(() =>
+            {
+                Console.WriteLine("Task run 2: " + Thread.CurrentThread.ManagedThreadId);
+            }).ConfigureAwait(false);
+            // продолжение выполняется в потоке из ThreadPool
+            Assert.AreNotEqual(mainThreadId, Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine($"Продолжение2: {Thread.CurrentThread.ManagedThreadId}, {SynchronizationContext.Current}\n");
+        });
+    }
+    
+    [TestMethod]
+    public async Task вызов_post_занимает_много_времени()
+    {
+        AsyncContext.Run(async () =>
+        {
+            var mainThreadId = Thread.CurrentThread.ManagedThreadId;
+            var mainContext = SynchronizationContext.Current;
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var sum = await GetSum(() =>
+            {
+                Assert.AreEqual(mainThreadId, Thread.CurrentThread.ManagedThreadId);
+                Assert.AreEqual(SynchronizationContext.Current, mainContext);
+            });
+            stopwatch.Stop();
+            Console.WriteLine($"Time: {stopwatch.ElapsedMilliseconds}");
+            
+            Assert.AreEqual(1784293664, sum);
+        });
+    }
+    
+    [TestMethod]
+    public async Task без_вызова_post_работает_быстрее()
+    {
+        var mainThreadId = Thread.CurrentThread.ManagedThreadId;
+        Assert.IsNull(SynchronizationContext.Current);
+
+        var stopwatch = new Stopwatch();
+        stopwatch.Start();
+        var sum = await GetSum(() =>
+        {
+            Assert.AreNotEqual(mainThreadId, Thread.CurrentThread.ManagedThreadId);
+            Assert.IsNull(SynchronizationContext.Current);
+        });
+        stopwatch.Stop();
+        Console.WriteLine($"Time: {stopwatch.ElapsedMilliseconds}");
+            
+        Assert.AreEqual(1784293664, sum);
+    }
+
+    private static async Task<int> GetSum(Action action)
+    {
+        var sum = 0;
+        foreach (var i in Enumerable.Range(1, 1000000))
+        {
+            var val = await Task.Run(() =>
+            {
+                Assert.IsNull(SynchronizationContext.Current);
+                return i;
+            });
+            action();
+            sum += val;
+        }
+
+        return sum;
     }
 }
 
