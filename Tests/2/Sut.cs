f9a2a2cf2a707bdeab680c713805650a79e12b13
Riabov.AA <riabov.aa@parmalogica.ru>
2024-03-07 09:34:52 +0500

    реализация_асинхронного_интерфейса_синхронно

---
+++ Tests/2/Sut.cs
@@ -0,0 +1,36 @@
+﻿namespace Tests._2;
+
+public interface IWebService
+{
+    Task<string> GetStringAsync();
+}
+
+public class Sut
+{
+    private readonly IWebService _webService;
+
+    public Sut(IWebService webService)
+    {
+        _webService = webService;
+    }
+
+    public async Task<string> DownloadStringWithRetries()
+    {
+        // Повторить попытку через 1 секунду, потом через 2 и через 4 секунды.
+        TimeSpan nextDelay = TimeSpan.FromSeconds(1);
+        for (int i = 0; i < 3; ++i)
+        {
+            try
+            {
+                return await _webService.GetStringAsync();
+            }
+            catch
+            {
+            }
+            await Task.Delay(nextDelay);
+            nextDelay = nextDelay + nextDelay;
+        }
+        // Попробовать в последний раз и разрешить распространение ошибки.
+        return await _webService.GetStringAsync();
+    }
+}
---
+++ Tests/2/Tests.cs
@@ -0,0 +1,58 @@
+﻿using Moq;
+
+namespace Tests._2;
+
+[TestClass]
+public class Tests
+{
+    [TestMethod]
+    public async Task retry_web_service()
+    {
+        var fakeWebService = new FakeWebService();
+        var sut = new Sut(fakeWebService);
+        
+        var func = () => sut.DownloadStringWithRetries();
+        
+        await Assert.ThrowsExceptionAsync<TimeoutException>(func);
+        Assert.AreEqual(4, fakeWebService.GetStringAsyncCallTimes);
+    }
+    
+    [TestMethod]
+    public async Task реализация_асинхронного_интерфейса_синхронно()
+    {
+        var syncWebService = new SyncWebService();
+
+        Task<string> task = null;
+        var func = () => task = syncWebService.GetStringAsync();
+        
+        await Assert.ThrowsExceptionAsync<TimeoutException>(func);
+        Assert.AreEqual(true, task.IsFaulted);
+        Assert.AreEqual(TaskStatus.Faulted, task.Status);
+    }
+}
+
+public class SyncWebService : IWebService
+{
+    public async Task<string> GetStringAsync()
+    {
+        try
+        {
+            throw new TimeoutException();
+        }
+        catch (Exception e)
+        {
+            return await Task.FromException<string>(e);
+        }
+    }
+}
+
+public class FakeWebService : IWebService
+{
+    public int GetStringAsyncCallTimes { get; set; }
+    public async Task<string> GetStringAsync()
+    {
+        GetStringAsyncCallTimes++;
+        await Task.Delay(100);
+        throw new TimeoutException();
+    }
+}
