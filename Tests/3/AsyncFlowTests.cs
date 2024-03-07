89d2cdf87900a3d9855390a782c529e2513e982f
Riabov.AA <riabov.aa@parmalogica.ru>
2024-03-07 12:25:54 +0500

    IAsyncEnumerable

---
+++ Tests/3/AsyncFlowTests.cs
@@ -0,0 +1,56 @@
+﻿namespace Tests._3;
+
+[TestClass]
+public class AsyncFlowTests
+{
+    [TestMethod]
+    public async Task sync_enumerable()
+    {
+        var sut = new Sut();
+        foreach (var value in sut.GetValues())
+        {
+            Console.WriteLine(value);
+        }
+
+        var res = sut.GetValues().GetEnumerator();
+
+        res.MoveNext();
+        Assert.AreEqual(10, res.Current);
+        res.MoveNext();
+        Assert.AreEqual(13, res.Current);
+        CollectionAssert.AreEqual(new []{10, 13}, sut.GetValues().ToArray());
+    }
+    
+    [TestMethod]
+    public async Task async_enumerable()
+    {
+        var sut = new Sut();
+        await foreach (var value in sut.GetValuesAsync())
+        {
+            Console.WriteLine(value);
+        }
+
+        var res = sut.GetValuesAsync().GetAsyncEnumerator();
+
+        var valueTask1 = res.MoveNextAsync();
+        await valueTask1;
+        Assert.IsInstanceOfType(valueTask1, typeof(ValueTask<bool>));
+        Assert.AreEqual(10, res.Current);
+        
+        var valueTask2 = res.MoveNextAsync();
+        await valueTask2;
+        Assert.IsInstanceOfType(valueTask2, typeof(ValueTask<bool>));
+        Assert.AreEqual(13, res.Current);
+    }
+    
+    [TestMethod]
+    public async Task получения_данных_из_внешнего_хранилища()
+    {
+        var sut = new Sut();
+        IAsyncEnumerable<string> data = sut.GetDataAsync();
+        await foreach (var name in data)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
---
+++ Tests/3/Sut.cs
@@ -0,0 +1,29 @@
+﻿namespace Tests._3;
+
+public class Sut
+{
+    public IEnumerable<int> GetValues()
+    {
+        yield return 10;
+        yield return 13;
+    }
+    
+    public async IAsyncEnumerable<int> GetValuesAsync()
+    {
+        await Task.Delay(10); // Асинхронная работа
+        yield return 10;
+        await Task.Delay(10); // Другая асинхронная работа
+        yield return 13;
+    }
+    
+    public async IAsyncEnumerable<string> GetDataAsync()
+    {
+        string[] data = { "Tom", "Sam", "Kate", "Alice", "Bob" };
+        for (int i = 0; i < data.Length; i++)
+        {
+            Console.WriteLine($"Получаем {i + 1} элемент");
+            await Task.Delay(500);
+            yield return data[i];
+        }
+    }
+}
