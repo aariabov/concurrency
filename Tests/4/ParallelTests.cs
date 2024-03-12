72cd26b3535da2a42de54f938ea671a12b428cd9
Riabov.AA <riabov.aa@parmalogica.ru>
2024-03-12 15:27:48 +0500

    Parallel.ForEach

---
+++ Tests/4/ParallelTests.cs
@@ -0,0 +1,77 @@
+﻿namespace Tests._4;
+
+[TestClass]
+public class ParallelTests
+{
+    [TestMethod]
+    public void find_prime_numbers_example()
+    {
+        var sut = new Sut();
+        var numbers = new [] { 1, 2, 3, 4 };
+
+        var result = sut.FindPrimeNumbers(numbers);
+        
+        CollectionAssert.AreEqual(new long[]{ 2, 3, 5, 7 }, result);
+    }
+    
+    [TestMethod]
+    public void find_prime_numbers()
+    {
+        var sut = new Sut();
+        var numbers = Enumerable.Range(1000, 1000).ToArray();
+
+        var watch = System.Diagnostics.Stopwatch.StartNew();
+        var result = sut.FindPrimeNumbers(numbers);
+        watch.Stop();
+        Console.WriteLine($"FindPrimeNumbers time: {watch.ElapsedMilliseconds}");
+    }
+    
+    [TestMethod]
+    public void find_prime_numbers_parallel()
+    {
+        var sut = new Sut();
+        var numbers = Enumerable.Range(1000, 1000).ToArray();
+
+        var watch = System.Diagnostics.Stopwatch.StartNew();
+        var result = sut.FindPrimeNumbersParallel(numbers);
+        watch.Stop();
+        Console.WriteLine($"FindPrimeNumbersParallel time: {watch.ElapsedMilliseconds}");
+    }
+    
+    [TestMethod]
+    public void find_prime_numbers_parallel_with_break()
+    {
+        var sut = new Sut();
+        var numbers = Enumerable.Range(1000, 1000).ToArray();
+
+        var watch = System.Diagnostics.Stopwatch.StartNew();
+        var result = sut.FindPrimeNumbersParallelWithBreak(numbers);
+        watch.Stop();
+        Console.WriteLine($"result count: {result.Length}");
+        Console.WriteLine($"FindPrimeNumbersParallelWithBreak time: {watch.ElapsedMilliseconds}");
+    }
+    
+    [TestMethod]
+    public void find_prime_numbers_parallel_with_cancel()
+    {
+        var sut = new Sut();
+        var numbers = Enumerable.Range(1000, 1000).ToArray();
+        var cts = new CancellationTokenSource(100);
+
+        var func = () => sut.FindPrimeNumbersParallelWithCancel(numbers, cts.Token);
+        Assert.ThrowsException<OperationCanceledException>(func);
+    }
+    
+    [TestMethod]
+    public void find_prime_numbers_parallel_with_lock()
+    {
+        var sut = new Sut();
+        var numbers = Enumerable.Range(1000, 2000).ToArray();
+
+        var watch = System.Diagnostics.Stopwatch.StartNew();
+        var result = sut.FindPrimeNumbersParallelWithLock(numbers);
+        watch.Stop();
+        Console.WriteLine($"result count: {result.Length}");
+        Console.WriteLine($"FindPrimeNumbersParallelWithLock time: {watch.ElapsedMilliseconds}");
+    }
+}
---
+++ Tests/4/Sut.cs
@@ -0,0 +1,105 @@
+﻿namespace Tests._4;
+
+public class Sut
+{
+    public long[] FindPrimeNumbers(int[] numbers)
+    {
+        var result = new List<long>();
+        foreach (var number in numbers)
+        {
+            result.Add(FindPrimeNumber(number));
+        }
+
+        return result.ToArray();
+    }
+    
+    public long[] FindPrimeNumbersParallel(int[] numbers)
+    {
+        var result = new List<long>();
+        var threads = new HashSet<int>();
+        Parallel.ForEach(numbers, number =>
+        {
+            var threadId = Thread.CurrentThread.ManagedThreadId;
+            threads.Add(threadId);
+            result.Add(FindPrimeNumber(number));
+        });
+        Console.WriteLine($"Used thread ids: {string.Join(',', threads)}");
+        return result.ToArray();
+    }
+    
+    public long[] FindPrimeNumbersParallelWithBreak(int[] numbers)
+    {
+        var result = new List<long>();
+        Parallel.ForEach(numbers, (number, state) =>
+        {
+            if (number > 1500)
+            {
+                state.Stop();
+            }
+            else
+            {
+                result.Add(FindPrimeNumber(number));
+            }
+        });
+        return result.ToArray();
+    }
+    
+    public long[] FindPrimeNumbersParallelWithCancel(int[] numbers, CancellationToken cancellationToken)
+    {
+        var result = new List<long>();
+        Parallel.ForEach(numbers, new ParallelOptions{CancellationToken = cancellationToken}, number =>
+        {
+            result.Add(FindPrimeNumber(number));
+        });
+        return result.ToArray();
+    }
+    
+    public long[] FindPrimeNumbersParallelWithLock(int[] numbers)
+    {
+        var result = new List<long>();
+        var mutex = new object();
+        var counter = 0;
+
+        Parallel.ForEach(numbers, number =>
+        {
+            lock (mutex)
+            {
+                counter++;
+                result.Add(FindPrimeNumber(number));
+            }
+        });
+        Console.WriteLine($"counter: {counter}");
+        return result.ToArray();
+    }
+
+    /// <summary>
+    /// Находит n-ое простое число
+    /// </summary>
+    /// <param name="n"></param>
+    /// <returns></returns>
+    private long FindPrimeNumber(int n)
+    {
+        int count=0;
+        long a = 2;
+        while(count<n)
+        {
+            long b = 2;
+            int prime = 1;// to check if found a prime
+            while(b * b <= a)
+            {
+                if(a % b == 0)
+                {
+                    prime = 0;
+                    break;
+                }
+                b++;
+            }
+            if(prime > 0)
+            {
+                count++;
+            }
+            a++;
+        }
+        return (--a);
+    }
+}
