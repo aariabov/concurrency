8b5780cd55013828a8b92ca839781014a8c20a37
Riabov.AA <riabov.aa@parmalogica.ru>
2024-03-27 15:14:02 +0500

    dataflow and observable

---
+++ Tests/8/DataflowExtensions.cs
@@ -0,0 +1,93 @@
+﻿using System.Runtime.CompilerServices;
+using System.Threading.Channels;
+using System.Threading.Tasks.Dataflow;
+
+namespace Tests._8;
+
+public static class DataflowExtensions
+{
+    public static bool TryReceiveItem<T>(this ISourceBlock<T> block, out T value)
+    {
+        if (block is IReceivableSourceBlock<T> receivableSourceBlock)
+        {
+            return receivableSourceBlock.TryReceive(out value);
+        }
+        
+        try
+        {
+            value = block.Receive(TimeSpan.Zero);
+            return true;
+        }
+        catch (TimeoutException)
+        {
+            // На данный момент доступного элемента нет.
+            value = default;
+            return false;
+        }
+        catch (InvalidOperationException)
+        {
+            // Блок завершен, элементов больше нет.
+            value = default;
+            return false;
+        }
+    }
+    public static async IAsyncEnumerable<T> ReceiveAllAsync<T>(this ISourceBlock<T> block,
+        [EnumeratorCancellation] CancellationToken cancellationToken = default)
+    {
+        while (await block.OutputAvailableAsync(cancellationToken).ConfigureAwait(false))
+        {
+            while (block.TryReceiveItem(out var value))
+            {
+                yield return value;
+            }
+        }
+    }
+    
+    public static async Task WriteToBlockAsync<T>(this IAsyncEnumerable<T> enumerable,
+        ITargetBlock<T> block, CancellationToken token = default)
+    {
+        try
+        {
+            await foreach (var item in enumerable.WithCancellation(token).ConfigureAwait(false))
+            {
+                await block.SendAsync(item, token).ConfigureAwait(false);
+            }
+            block.Complete();
+        }
+        catch (Exception ex)
+        {
+            block.Fault(ex);
+        }
+    }
+    
+    public static async IAsyncEnumerable<T> ToCustomAsyncEnumerable<T>(this IObservable<T> observable)
+    {
+        Channel<T> buffer = Channel.CreateUnbounded<T>();
+        using (observable.Subscribe(
+                   value => buffer.Writer.TryWrite(value),
+                   error => buffer.Writer.Complete(error),
+                   () => buffer.Writer.Complete()))
+        {
+            await foreach (T item in buffer.Reader.ReadAllAsync())
+                yield return item;
+        }
+    }
+    
+    // ПРЕДУПРЕЖДЕНИЕ: возможна потеря элементов; см. обсуждение!
+    public static async IAsyncEnumerable<T> ToBufferAsyncEnumerable<T>(this IObservable<T> observable, int bufferSize)
+    {
+        var bufferOptions = new BoundedChannelOptions(bufferSize)
+        {
+            FullMode = BoundedChannelFullMode.DropOldest,
+        };
+        Channel<T> buffer = Channel.CreateBounded<T>(bufferOptions);
+        using (observable.Subscribe(
+                   value => buffer.Writer.TryWrite(value),
+                   error => buffer.Writer.Complete(error),
+                   () => buffer.Writer.Complete()))
+        {
+            await foreach (T item in buffer.Reader.ReadAllAsync())
+                yield return item;
+        }
+    }
+}
--- Tests/8/InteractionTests.cs
+++ Tests/8/InteractionTests.cs
@@ -1,5 +1,8 @@
-﻿using System.Net;
+﻿using System.Diagnostics;
+using System.Net;
 using System.Reactive.Linq;
+using System.Threading.Tasks.Dataflow;
+using Microsoft.VisualBasic;
 
 namespace Tests._8;
 
@@ -121,6 +124,101 @@ public class InteractionTests
         await Task.Delay(1100);
         Assert.AreEqual(4, client.GetAsyncCallTimes);
     }
+    
+    [TestMethod]
+    public async Task data_flow_as_async()
+    {
+        var multiplyBlock = new TransformBlock<int, int>(value => value * 2);
+        multiplyBlock.Post(5);
+        multiplyBlock.Post(2);
+        multiplyBlock.Complete();
+        var result = multiplyBlock.ReceiveAllAsync();
+        Assert.AreEqual(10, await result.FirstAsync());
+        Assert.AreEqual(4, await result.LastAsync());
+        // await foreach (int item in multiplyBlock.ReceiveAllAsync())
+        // {
+        //     Console.WriteLine(item);
+        // }
+    }
+    
+    [TestMethod]
+    public async Task buffer_block_as_observable()
+    {
+        var buffer = new BufferBlock<int>();
+        IObservable<int> integers = buffer.AsObservable();
+        integers.Subscribe(data =>
+            {
+                Trace.WriteLine(data);
+                Assert.AreEqual(13, data);
+            },
+            ex => Trace.WriteLine(ex),
+            () => Trace.WriteLine("Done"));
+        buffer.Post(13);
+        await Task.Delay(10);
+    }
+    
+    [TestMethod]
+    public async Task action_block_as_a_observer()
+    {
+        IObservable<DateTimeOffset> ticks =
+            Observable.Interval(TimeSpan.FromSeconds(1))
+                .Timestamp()
+                .Select(x => x.Timestamp)
+                .Take(5);
+        
+        var display = new ActionBlock<DateTimeOffset>(x => Trace.WriteLine(x));
+        ticks.Subscribe(display.AsObserver());
+        try
+        {
+            display.Completion.Wait();
+            Trace.WriteLine("Done.");
+        }
+        catch (Exception ex)
+        {
+            Trace.WriteLine(ex);
+        }
+    }
+
+    [TestMethod]
+    public async Task observable_to_async_enumerable()
+    {
+        IObservable<long> observable = Observable.Interval(TimeSpan.FromSeconds(1));
+        // ПРЕДУПРЕЖДЕНИЕ: может потреблять неограниченную память
+        IAsyncEnumerable<long> enumerable = observable.ToAsyncEnumerable().Take(3);
+        await foreach (var e in enumerable)
+        {
+            Trace.WriteLine(e);
+        }
+    }
+
+    [TestMethod]
+    public async Task observable_to_custom_async_enumerable()
+    {
+        IObservable<long> observable = Observable.Interval(TimeSpan.FromSeconds(1));
+        // ПРЕДУПРЕЖДЕНИЕ: может потреблять неограниченную память
+        IAsyncEnumerable<long> enumerable = observable.ToCustomAsyncEnumerable().Take(3);
+        await foreach (var e in enumerable)
+        {
+            Trace.WriteLine(e);
+        }
+    }
+
+    [TestMethod]
+    public async Task observable_to_buffer_async_enumerable()
+    {
+        IObservable<long> observable = Observable.Interval(TimeSpan.FromSeconds(1));
+        // ПРЕДУПРЕЖДЕНИЕ: потеря елементов, тк продьюсер производит элементы быстрее,
+        // чем консамер может их потребить
+        IAsyncEnumerable<long> enumerable = observable
+            .ToBufferAsyncEnumerable(2)
+            .Take(5);
+        await foreach (var e in enumerable)
+        {
+            await Task.Delay(2000);
+            Trace.WriteLine(e);
+        }
+    }
+
 }
     
 public class MockHttpClient : HttpClient
