007598be4de4422eed3260b7171564977799dc4e
Riabov.AA <riabov.aa@parmalogica.ru>
2024-04-15 23:30:01 +0500

    creating and canceling Task

---
+++ Tests/ShaktiTanwar/CancelTaskTests.cs
@@ -0,0 +1,63 @@
+﻿using System.Net;
+
+namespace Tests.ShaktiTanwar;
+
+[TestClass]
+public class CancelTaskTests
+{
+    [TestMethod]
+    public async Task cancel_web_client()
+    {
+        CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+        CancellationToken token = cancellationTokenSource.Token;
+
+        DownloadFileWithToken(token);
+        await Task.Delay(200);
+        cancellationTokenSource.Cancel();
+        await Task.Delay(200);
+    }
+    
+    private static void DownloadFileWithToken(CancellationToken token)
+    {
+        WebClient webClient = new WebClient();
+        //Here we are registering callback delegate that will get called as soon as user cancels token
+        token.Register(() => webClient.CancelAsync());
+
+        webClient.DownloadStringAsync(new Uri("http://www.google.com"));
+        webClient.DownloadStringCompleted += (sender, e) => {
+            if (!e.Cancelled)
+            {
+                Console.WriteLine("Download Complete.");
+            }
+            else
+            {
+                Console.WriteLine("Download Cancelled.");
+            }
+        };
+    }
+    
+    [TestMethod]
+    public async Task cancel_long_loop()
+    {
+        var cancellationTokenSource = new CancellationTokenSource(500);
+        var token = cancellationTokenSource.Token;
+        
+        var func = () => LongLoop(token);
+        await Assert.ThrowsExceptionAsync<TaskCanceledException>(func);
+    }
+
+    private async Task LongLoop(CancellationToken token)
+    {
+        for (int i = 0; i < 1000; i++)
+        {
+            Console.WriteLine($"Iter {i}");
+            await Task.Delay(100, token);
+
+            if (token.IsCancellationRequested)
+            {
+                Console.WriteLine($"Task was canceled");
+                token.ThrowIfCancellationRequested();
+            }
+        }
+    }
+}
---
+++ Tests/ShaktiTanwar/CreatingTaskTests.cs
@@ -0,0 +1,35 @@
+﻿namespace Tests.ShaktiTanwar;
+
+[TestClass]
+public class CreatingTaskTests
+{
+    [TestMethod]
+    public async Task create_task_example()
+    {
+        new Task(() => Console.WriteLine($"new Task started")).Start();
+        await Task.Factory.StartNew(() => Console.WriteLine($"Task.Factory.StartNew started"));
+        await Task.Run(() => Console.WriteLine($"Task.Run started"));
+        var delayTask = Task.Delay(10);
+        await delayTask;
+
+        var resultTask = Task.FromResult(42);
+        Console.WriteLine($"Task.FromResult: {await resultTask}");
+
+        var func = () => Task.FromException(new Exception("fake"));
+        await Assert.ThrowsExceptionAsync<Exception>(func);
+    }
+    
+    [TestMethod]
+    public async Task task_yield_example()
+    {
+        for (int i = 0; i < 10000; i++)
+        {
+            if (i % 1000 == 0)
+            {
+                Helpers.PrintThreadInfo("Before Task.Yield()");
+                await Task.Yield();
+                Helpers.PrintThreadInfo("After Task.Yield()");
+            }
+        }
+    }
+}
