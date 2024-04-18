1ab9a19686679c4f907e8c74c1fe39376cfd9e80
Riabov.AA <riabov.aa@parmalogica.ru>
2024-04-18 07:34:59 +0500

    eap

---
+++ Tests/ShaktiTanwar/EventBasedAsynchronousTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+
+namespace Tests.ShaktiTanwar;
+
+[TestClass]
+public class EventBasedAsynchronousTests
+{
+    [TestMethod]
+    public async Task eap_to_task()
+    {
+        var result = await Task.Run(() =>
+        {
+            var taskCompletionSource = new TaskCompletionSource<string>();
+            var webClient = new WebClient();
+            webClient.DownloadStringCompleted += (s, e) =>
+            {
+                if (e.Error != null)
+                    taskCompletionSource.TrySetException(e.Error);
+                else if (e.Cancelled)
+                    taskCompletionSource.TrySetCanceled();
+                else
+                    taskCompletionSource.TrySetResult(e.Result);
+            };
+            webClient.DownloadStringAsync(new Uri("https://google.com"));
+            return taskCompletionSource.Task;
+        });
+
+        Assert.IsTrue(result.Length > 0);
+    }
+}
