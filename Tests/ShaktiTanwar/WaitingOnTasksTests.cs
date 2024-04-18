a9591ac1cab04ca48bd80729416bc59e95edaf5f
Riabov.AA <riabov.aa@parmalogica.ru>
2024-04-18 08:06:49 +0500

    WaitingOnTasksTests

---
+++ Tests/ShaktiTanwar/WaitingOnTasksTests.cs
@@ -0,0 +1,73 @@
+﻿namespace Tests.ShaktiTanwar;
+
+[TestClass]
+public class WaitingOnTasksTests
+{
+    [TestMethod]
+    public async Task when_any()
+    {
+        Task taskA = Task.Factory.StartNew(() =>
+        {
+            Task.Delay(10).Wait();
+            Console.WriteLine("TaskA finished");
+        });
+        Task taskB = Task.Factory.StartNew(() =>
+        {
+            Task.Delay(50).Wait();
+            Console.WriteLine("TaskB finished");
+        });
+        Task.WhenAny(taskA, taskB).Wait();
+        Console.WriteLine("Calling method finishes");
+    }
+
+    [TestMethod]
+    public async Task when_all()
+    {
+        Task taskA = Task.Factory.StartNew(() =>
+        {
+            Task.Delay(10).Wait();
+            Console.WriteLine("TaskA finished");
+        });
+        Task taskB = Task.Factory.StartNew(() =>
+        {
+            Task.Delay(50).Wait();
+            Console.WriteLine("TaskB finished");
+        });
+        Task.WhenAll(taskA, taskB).Wait();
+        Console.WriteLine("Calling method finishes");
+    }
+
+    [TestMethod]
+    public async Task wait_any()
+    {
+        Task taskA = Task.Factory.StartNew(() =>
+        {
+            Task.Delay(10).Wait();
+            Console.WriteLine("TaskA finished");
+        });
+        Task taskB = Task.Factory.StartNew(() =>
+        {
+            Task.Delay(50).Wait();
+            Console.WriteLine("TaskB finished");
+        });
+        Task.WaitAny(taskA, taskB);
+        Console.WriteLine("Calling method finishes");
+    }
+
+    [TestMethod]
+    public async Task wait_all()
+    {
+        Task taskA = Task.Factory.StartNew(() =>
+        {
+            Task.Delay(10).Wait();
+            Console.WriteLine("TaskA finished");
+        });
+        Task taskB = Task.Factory.StartNew(() =>
+        {
+            Task.Delay(50).Wait();
+            Console.WriteLine("TaskB finished");
+        });
+        Task.WaitAll(taskA, taskB);
+        Console.WriteLine("Calling method finishes");
+    }
+}
