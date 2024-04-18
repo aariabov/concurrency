661a41a801abd9371c79c369d9d608ce1912b983
Riabov.AA <riabov.aa@parmalogica.ru>
2024-04-18 07:59:19 +0500

    AttachedTasksTests

---
+++ Tests/ShaktiTanwar/AttachedTasksTests.cs
@@ -0,0 +1,37 @@
+﻿namespace Tests.ShaktiTanwar;
+
+[TestClass]
+public class AttachedTasksTests
+{
+    [TestMethod]
+    public async Task detached_task()
+    {
+        Task parentTask = Task.Factory.StartNew(() =>
+        {
+            Console.WriteLine("Parent task started");
+
+            Task childTask = Task.Factory.StartNew(() => {
+                Console.WriteLine("Child task started");
+            });
+            Console.WriteLine("Parent task Finish");
+        });
+        parentTask.Wait();
+        Console.WriteLine("Work Finished");
+    }
+    
+    [TestMethod]
+    public async Task attached_task()
+    {
+        Task parentTask = Task.Factory.StartNew(() =>
+        {
+            Console.WriteLine("Parent task started");
+
+            Task childTask = Task.Factory.StartNew(() => {
+                Console.WriteLine("Child task started");
+            },TaskCreationOptions.AttachedToParent);
+            Console.WriteLine("Parent task Finish");
+        });
+        parentTask.Wait(); // родительская задача также ждет дочернюю
+        Console.WriteLine("Work Finished");
+    }
+}
